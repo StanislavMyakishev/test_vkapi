1a2020807a46642f460a5bb92ad9c35ba443d565
StanislavMyakishev <masiff-s@live.ru>
2019-09-18 17:48:29 +0300

    UI inprv

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ function drawAppearence(resp){
             for (let i = 0; i < friends.length; ++i){
                 let f = friends[i];
                 html += `<li class="list-group-item d-flex justify-content-between align-items-center">
-                            <div style="align:left;>
-                                <img class="friend-img" src="${f.photo_100}" />
-                            </div>                            
-                            <div style="align:right;>
-                                <a target="_blank" href="https://vk.com/id${f.id}">
-                                <h4 align=center>${f.first_name} ${f.last_name}</h4>
+                        <a target="_blank" href="https://vk.com/id${f.id}">
+                            <div class="list-elem-img">
+                                <img class="friend-img" src="${f.photo_100}"/>
+                            </div>
+                            <div class="list-elem-name">
+                                <h4>${f.first_name} ${f.last_name}</h4>
                             </div>
                         </a>
                     </li>`;
