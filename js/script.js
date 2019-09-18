39eee492053732614d605129ddf1368066b231f1
StanislavMyakishev <masiff-s@live.ru>
2019-09-18 22:10:12 +0300

    material icons

--- js/script.js
+++ js/script.js
@@ -17,18 +17,20 @@ function drawAppearence(resp){
             for (let i = 0; i < friends.length; ++i){
                 let f = friends[i];
                 let online = '';
+                let mobile = 'none';
                 if (f.online === 1) {
                     online = 'Online';
                 }
                 if (f.online_mobile === 1) {
-                    online = 'Online mobile';
+                    mobile = 'initial';
                 }
                 html += `<li class="list-group-item d-flex justify-content-between align-items-center">
                         <a target="_blank" href="https://vk.com/id${f.id}">
                             <div class="list-elem">
                                 <img class="list-elems friend-img" src="${f.photo_100}"/>
                                 <h4 class="list-elems friend-text">${f.first_name} ${f.last_name}</h4>
-                                <p class="online-status">${online}</>
+                                <p class="online-status">${online}</p>
+                                <i style="display="${mobile}"; class="tiny material-icons">phone_iphone</i>w
                             </div>
                         </a>
                     </li>`;
